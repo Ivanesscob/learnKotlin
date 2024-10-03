f1f62937f08229096c03ec5b96d1fcf537125c32
Ivanesscob <Ivanesscob@gmail.com>
2024-10-04 02:07:52 +0300

    3

--- SecondTaskArrays/thirdTask/src/Main.kt
+++ SecondTaskArrays/thirdTask/src/Main.kt
@@ -16,5 +16,90 @@
 
 
 fun main() {
+    val keyWord = readln()
+    val message = readln()
+    var result = ""
+    val lenghtOfKeyWord = keyWord.length
+    val lenghtOfMessaage = message.length
+    var counterKeyWord = 0
+    val array = arrayOf(
+        arrayOf('А', 21),
+        arrayOf('Б', 13),
+        arrayOf('В', 4),
+        arrayOf('Г', 20),
+        arrayOf('Д', 22),
+        arrayOf('Е', 1),
+        arrayOf('Ё', 25),
+        arrayOf('Ж', 12),
+        arrayOf('З', 24),
+        arrayOf('И', 14),
+        arrayOf('Й', 2),
+        arrayOf('К', 28),
+        arrayOf('Л', 9),
+        arrayOf('М', 23),
+        arrayOf('Н', 3),
+        arrayOf('О', 29),
+        arrayOf('П', 6),
+        arrayOf('Р', 16),
+        arrayOf('С', 15),
+        arrayOf('Т', 11),
+        arrayOf('У', 26),
+        arrayOf('Ф', 5),
+        arrayOf('Х', 30),
+        arrayOf('Ц', 27),
+        arrayOf('Ч', 8),
+        arrayOf('Ш', 18),
+        arrayOf('Щ', 10),
+        arrayOf('Ь', 33),
+        arrayOf('Ы', 31),
+        arrayOf('Ъ', 32),
+        arrayOf('Э', 19),
+        arrayOf('Ю', 7),
+        arrayOf('Я', 17)
+    )
+    for (i in 0 until lenghtOfMessaage){
+        var numberOfLetterOfWord = 0
+        var numberOfLetterOfKeyWord = 0
+
+        if (counterKeyWord > lenghtOfKeyWord-1){
+            counterKeyWord = 0
+        }
+
+        for (j in 0 until 33){
+            if (message[i] == array[j][0]){
+                numberOfLetterOfWord = array[j][1] as Int
+
+            }
+            if(keyWord[counterKeyWord] == array[j][0]){
+                numberOfLetterOfKeyWord = array[j][1] as Int
+
+            }
+
+        }
+        for(j in 0 until 33){
+            if(numberOfLetterOfWord+numberOfLetterOfKeyWord > 33){
+                if (array[j][1] == numberOfLetterOfWord+numberOfLetterOfKeyWord-33){
+                    result+= array[j][0]
+                    break
+                }
+            }
+            else{
+                if (array[j][1] == numberOfLetterOfWord+numberOfLetterOfKeyWord){
+                    result+= array[j][0]
+                    break
+                }
+            }
+
+        }
+        counterKeyWord++
+
+
+
+
+
+    }
+    println(result)
+
+
 
 }
