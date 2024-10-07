74e1b2b46b2a84a90ab465b4014ba66ba04dd545
Ivanesscob <Ivanesscob@gmail.com>
2024-10-07 16:43:06 +0300

    5

--- SecondTaskArrays/fifthTask/src/Main.kt
+++ SecondTaskArrays/fifthTask/src/Main.kt
@@ -4,8 +4,29 @@
 Получаем группы:
 "ate", "eat", "tea"
 "nat", "tan"
-"bat"*/
+"bat"
+*/
 
 fun main() {
-    
+    val string = readln()
+    val arrOfString = string.split(" ")
+    var letters = mutableMapOf<String, MutableList<String>>()
+    for (element in arrOfString){
+        var sp = element
+        var sp1 = sp.toCharArray().sorted().joinToString("")
+        if (letters.containsKey(sp1)) {
+            letters[sp1]?.add(sp)
+        } else {
+
+            letters[sp1] = mutableListOf(sp)
+        }
+
+    }
+    for (valueList in letters.values) {
+        for (value in valueList) {
+            print("\"$value\", ")
+        }
+        println()
+    }
 }
+
